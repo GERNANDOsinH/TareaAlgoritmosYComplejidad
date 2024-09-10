5fd206d900322a2ac4aab5dfc66bb21536ee9d1b
Fernando Hernandez <fernahernandez.c@gmail.com>
2024-09-09 22:21:33 -0300

    a
    a

--- sorterAlgorithms/selectionSort.py
+++ sorterAlgorithms/selectionSort.py
@@ -1,5 +1,5 @@
 def selectionSort(arr):
-    # Descripción: Algoritmo de ordenamiento selectionSort(creo) que ordena los elementos de un arreglo de entrada.
+    # Descripción: Algoritmo de ordenamiento selectionSort que ordena los elementos de un arreglo de entrada.
     for i in range(0,len(arr)):
         m = 0
         # Bucle que compara los elementos para encontrar al menor
